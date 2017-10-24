34c5de90717ff11b97a95f3a6828931ca46a331d
saibot777 <stefantrajkovic777@gmail.com>
2017-10-25 01:48:24 +0200

    register validation completed

--- ytb-planner-app/routes/users.js
+++ ytb-planner-app/routes/users.js
@@ -17,4 +17,29 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
+// Register Form
+router.post('/register', (req, res) => {
+    let errors = [];
+
+    if (req.body.password != req.body.passwordConfirm) {
+        errors.push({text: 'Passwords do not match'});
+    }
+
+    if (req.body.password.length < 4) {
+        errors.push({text: 'Password must be at least 4 characters'});
+    }
+
+    if (errors.length > 0) {
+        res.render('users/register', {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            passwordConfirm: req.body.passwordConfirm
+        });
+    } else {
+        res.send('passed');
+    }
+});
+
 module.exports = router;
