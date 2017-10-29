ba53b8a8ca9f8c51e403f1477f17847117b75cfc
saibot777 <stefantrajkovic777@gmail.com>
2017-10-29 02:17:24 +0200

    registering and checking existing email validation added

--- ytb-planner-app/routes/users.js
+++ ytb-planner-app/routes/users.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
 
-// Load Idea Model
+// Load Models
 require('../models/Idea');
 const Idea = mongoose.model('ideas');
+require('../models/User');
+const User = mongoose.model('users');
 
 
 // Login
@@ -38,7 +42,34 @@ router.post('/register', (req, res) => {
             passwordConfirm: req.body.passwordConfirm
         });
     } else {
-        res.send('passed');
+        User.findOne({email: req.body.email})
+            .then(user => {
+                if(user) {
+                    req.flash('error_msg', 'Email already exist!');
+                    res.redirect('/users/register');
+                } else {
+                    const newUser = new User({
+                        name: req.body.name,
+                        email: req.body.email,
+                        password: req.body.password
+                    });
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if (err) throw err; 
+                            newUser.password = hash;
+                            newUser.save()
+                                .then(user => {
+                                    req.flash('success_msg', 'You are now registered and can login in');
+                                    res.redirect('/users/login');
+                                })
+                                .catch(err => {
+                                    console.log(err);
+                                    return;
+                                });
+                        });
+                    });
+                }
+            });
     }
 });
 
