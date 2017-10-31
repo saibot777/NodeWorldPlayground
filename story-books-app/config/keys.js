550f25e83fb87e14ac545a93924d42c3e31f425b
saibot777 <stefantrajkovic777@gmail.com>
2017-10-31 15:53:46 +0100

    initial commit for story-books-app...

---
+++ story-books-app/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoUri: 'mongodb://stefan:1234@ds241055.mlab.com:41055/brad-frenzie',
+    googleCliendID: '201211273714-69i59qtob13qk6g49c1c8152sa1p3uii.apps.googleusercontent.com',
+    googleClientSecret: 'eo2AJ5tf8hYvYIy8zO89BDMV'
+}
