49e62314f6c37ffae173a0811ecb13ff6488a5d4
saibot777 <stefantrajkovic777@gmail.com>
2017-11-01 23:49:13 +0100

    UI update, footer, add button, about-page...

--- story-books-app/app.js
+++ story-books-app/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
@@ -52,6 +53,9 @@ app.use((req, res, next) => {
   res.locals.user = req.user || null;
   next();
 });
+
+// Set static path
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Use Routes
 app.use('/', index);
