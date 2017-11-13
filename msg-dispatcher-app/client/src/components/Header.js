1276253801da0e64c0b9fcaad2d02516efba33b7
saibot777 <stefantrajkovic777@gmail.com>
2017-11-13 04:22:04 +0100

    UI Polish...

--- msg-dispatcher-app/client/src/components/Header.js
+++ msg-dispatcher-app/client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     renderContent() {
@@ -20,7 +21,12 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <a className="left brand-logo">DispatherMSG</a>
+                    <Link 
+                        to={this.props.auth ? '/surveys' : '/'}
+                        style={{ marginLeft: '10px' }} 
+                        className="left brand-logo">
+                        DispatherMSG
+                    </Link>
                     <ul className="right">
                         {this.renderContent()}
                     </ul>
