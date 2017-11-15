93d728e6a2766c751b99ed1e806e18a0fd5492c7
saibot777 <stefantrajkovic777@gmail.com>
2017-11-15 04:38:19 +0100

    sub-model recepient added

--- msg-dispatcher-app/models/Survey.js
+++ msg-dispatcher-app/models/Survey.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const RecepientSchema = require('./Recepient');
 
 const surveySchema = new Schema({
     title: String,
     body: String,
     subject: String,
-    recepients: [String]
-})
+    recepients: [RecepientSchema],
+    yes: { type: Number, default: 0 },
+    no: { type: Number, default: 0 }
+});
 
 mongoose.model('surveys', surveySchema);
