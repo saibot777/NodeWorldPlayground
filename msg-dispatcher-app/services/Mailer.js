3e1e3b4ccdb4c277fc4249bde5f96d92bcd5268e
saibot777 <stefantrajkovic777@gmail.com>
2017-11-17 20:08:19 +0100

    addRecepient method created

--- msg-dispatcher-app/services/Mailer.js
+++ msg-dispatcher-app/services/Mailer.js
@@ -10,6 +10,33 @@ export default class Mailer extends helper.Mail {
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
         this.recepients = this.formatAddresses(recepients);
+
+        this.addContent(this.body);
+        this.addClickTracking();
+        this.addRecipients();
+    }
+
+    formatAddresses(recepients) {
+        return recepients.map(({ email }) => {
+            return new helper.Email(email);
+        });
+    }
+
+    addClickTracking() {
+        let trackingSettings = new helper.trackingSettings();
+        let clickTracking = new helper.ClickTracking(true, true);
+
+        trackingSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
+    }
+
+    addRecipients() {
+        let personalize = new helper.Personalization();
+        
+        this.recepients.forEach(recepient => {
+            personalize.addTo(recepient);
+        });
+        this.addPersonalization(personalize);
     }
 
 }
