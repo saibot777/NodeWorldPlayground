f020ec4480d7739afb6539a5e950c845b60e605a
saibot777 <stefantrajkovic777@gmail.com>
2017-11-17 19:51:21 +0100

    addContent and addClickTracking methods implemented...

--- msg-dispatcher-app/services/Mailer.js
+++ msg-dispatcher-app/services/Mailer.js
@@ -3,5 +3,13 @@ const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
 export default class Mailer extends helper.Mail {
+    constructor({ subject, recepients }, content) {
+        super();
+
+        this.from_email = new helper.Email('no-reply@dispatcher.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recepients = this.formatAddresses(recepients);
+    }
 
 }
