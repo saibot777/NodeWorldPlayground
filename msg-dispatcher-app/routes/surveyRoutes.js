0f9f0a35c3ffe04bdffb4adb1b382e278948aaff
saibot777 <stefantrajkovic777@gmail.com>
2017-11-17 20:48:22 +0100

    Mailer done on the server side, moving to client...

--- msg-dispatcher-app/routes/surveyRoutes.js
+++ msg-dispatcher-app/routes/surveyRoutes.js
@@ -7,6 +7,10 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
+    app.get('/api/surveys/thanks', (req, res) => {
+        res.send('Thanks for voting!');
+    });
+
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
         const { title, subject, body, recipients } = req.body;
 
@@ -20,5 +24,16 @@ module.exports = app => {
         }); 
 
         let mailer = new Mailer(survey, surveyTemplate(survey));
+
+        try {
+            await mailer.send();
+            await mailer.save();
+            req.user.credits -= 1;
+            let user = await req.user.save();
+    
+            res.send(user);
+        } catch (err) {
+            res.status(422).send(err);
+        }
     });
 }
