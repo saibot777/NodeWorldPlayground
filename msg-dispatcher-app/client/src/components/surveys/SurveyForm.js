b916fd027b8a6668d80da2d57107d63c3c24374f
saibot777 <stefantrajkovic777@gmail.com>
2017-11-19 15:40:19 +0100

    rendering fields in redux survey form, UI done also

--- msg-dispatcher-app/client/src/components/surveys/SurveyForm.js
+++ msg-dispatcher-app/client/src/components/surveys/SurveyForm.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import _ from 'lodash';
+import SurveyField from './SurveyField';
+import { Link } from 'react-router-dom';
+
+const FIELDS = [
+    { label: 'Syrvey Title', name: 'title' },
+    { label: 'Subject Line', name: 'subject' },
+    { label: 'Email Body', name: 'body' },
+    { label: 'Recipient List', name: 'emails' },
+]
 
 class SurveyForm extends Component {
+    renderFields() {
+        return _.map(FIELDS, ({ label, name }) => {
+            return <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+        })
+    }
+
     render() {
         return (
-            <div>
+            <div style={{marginTop: '15px'}}>
                 <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-                    <Field 
-                        type="text" 
-                        name="surveyTitle"
-                        component="input"
-                    />
-                    <button type="submit">Submit</button>
+                    {this.renderFields()}
+                    <Link
+                        to="/surveys"
+                        className="red btn-flat white-text">
+                        Cancel
+                    </Link>
+                    <button 
+                        className="teal btn-flat right white-text" 
+                        type="submit">
+                        Next
+                        <i className="material-icons right">done</i>
+                    </button>
                 </form>
             </div>
         );
