fc292523b34a7858318580337127f58c27afba9e
saibot777 <stefantrajkovic777@gmail.com>
2018-01-04 16:09:17 +0100

    automatic server reload, error handling and logging with morgan implemented...

--- rest-ecomerc/app.js
+++ rest-ecomerc/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
 
 // Routes
 const productRoutes = require('./api/routes/products');
@@ -7,5 +10,20 @@ app.use('/products', productRoutes);
 
 const orderRoutes = require('./api/routes/orders');
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
