cf874e970fee8a6ee656a3d5d37cbd62596f660c
saibot777 <stefantrajkovic777@gmail.com>
2018-01-05 15:41:47 +0100

    GET method for orders...

--- rest-ecomerc/api/routes/orders.js
+++ rest-ecomerc/api/routes/orders.js
@@ -5,9 +5,18 @@ const mongooose = require('mongoose');
 const Order = require('../models/Order');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /orders'
-    });
+    Order
+        .find()
+        .select('product quantity _id')
+        .exec()
+        .then(docs => {
+            res.status(200).json(docs)
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.post('/', (req, res, next) => {
