2e411e35be7cecdbccb8b422bb0fd779457f6285
saibot777 <stefantrajkovic777@gmail.com>
2018-10-28 17:06:51 +0100

    posting data

--- node-full-app/controllers/shop.js
+++ node-full-app/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -13,13 +14,13 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
-    res.render('shop/product-detail', { 
-      product, 
+    res.render('shop/product-detail', {
+      product: product,
       pageTitle: product.title,
       path: '/products'
     });
-  })
-}
+  });
+};
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -40,8 +41,11 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  console.log(prodId)
-}
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
+};
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
