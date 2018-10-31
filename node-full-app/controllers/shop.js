36282d7c2bfadce4e17292b3220223f675e09714
saibot777 <stefantrajkovic777@gmail.com>
2018-10-31 19:18:45 +0100

    fetching single product, refactoring...

--- node-full-app/controllers/shop.js
+++ node-full-app/controllers/shop.js
@@ -5,10 +5,10 @@ exports.getProducts = (req, res, next) => {
   Product
     .fetchAll()
     .then(([rows, fieldData]) => {
-      res.render('shop/index', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/'
+      res.render('shop/product-list', {
+        prods: rows,
+        pageTitle: 'All Products',
+        path: '/products'
       })
     })
     .catch(err => console.log(err));
@@ -16,13 +16,16 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    res.render('shop/product-detail', {
-      product: product,
-      pageTitle: product.title,
-      path: '/products'
-    });
-  });
+  Product
+    .findById(prodId)
+    .then(([product]) => {
+      res.render('shop/product-detail', {
+        product: product[0],
+        pageTitle: product.title,
+        path: '/products'
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
