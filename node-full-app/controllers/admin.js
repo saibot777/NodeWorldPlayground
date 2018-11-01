16e6965f8a47991d4e03cdd359864bb2d29a914d
saibot777 <stefantrajkovic777@gmail.com>
2018-11-01 13:36:27 +0100

    editing products admin section

--- node-full-app/controllers/admin.js
+++ node-full-app/controllers/admin.js
@@ -29,17 +29,20 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    if (!product) {
-      return res.redirect('/');
-    }
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing: editMode,
-      product: product
-    });
-  });
+  Product
+    .findById(prodId) 
+    .then(product => {
+      if (!product) {
+        return res.redirect('/');
+      }
+        res.render('admin/edit-product', {
+          pageTitle: 'Edit Product',
+          path: '/admin/edit-product',
+          editing: editMode,
+          product: product
+        });
+    })
+    .catch(err => console.log(err))
 };
 
 exports.postEditProduct = (req, res, next) => {
