e0eccd7add3e0192c1679452c3210767287e07be
saibot777 <stefantrajkovic777@gmail.com>
2018-12-10 23:28:31 +0100

    tours API UPDATE impl

--- typescript-api/api/v1/tours/controller.ts
+++ typescript-api/api/v1/tours/controller.ts
@@ -72,6 +72,40 @@ export const create: RequestHandler = (req: Request, res: Response) => {
 };
 
 /**
+ * @api {put} /tours/:id
+ *
+ * @apiName PUT Update Tour
+ *
+ * @apiHeader (RequestFileHeader) {String="application/json"} Content-Type
+ *
+ * @apiSuccess (200) Updated Tour
+ *
+ * @apiError (404) {String} Not Found
+ */
+
+export const update: RequestHandler = (req: Request, res: Response) => {
+    const tourID = req.params.id;
+    const tourIndex = DataStore.tours.findIndex((item: any) => item.id == tourID);
+    if (tourIndex > -1) {
+        const originalTour = DataStore.tours[tourIndex];
+        const newTour = {
+            id: tourID,
+            location: req.body.location || originalTour.location,
+            tourTitle: req.body.tourTitle || originalTour.tourTitle,
+            tourCategory: req.body.tourCategory || originalTour.tourCategory,
+            tourDescription: req.body.tourDescription || originalTour.tourDescription,
+            price: req.body.price || originalTour.price,
+            currency: req.body.currency || originalTour.currency
+        }
+        DataStore.tours[tourIndex] = newTour;
+        res.json({"status": "success", "message": "Tour updated"});
+    }
+    else {
+        res.json({"status": "error", "message": "Tour not found"});
+    }
+};
+
+/**
  * @api {delete} /tours/:id
  *
  * @apiName DELETE Removes Tour
