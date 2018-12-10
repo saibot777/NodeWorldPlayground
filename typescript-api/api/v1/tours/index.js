0ec7e1be9585155eafdbbc16b03eb247a4cb0a9e
saibot777 <stefantrajkovic777@gmail.com>
2018-12-10 13:19:01 +0100

    API refactoring

--- typescript-api/api/v1/tours/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var controller = require("./controller");
-var router = express.Router({ mergeParams: true });
-router.get('/', controller.getHomePage);
-module.exports = router;
--- typescript-api/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express = require("express");
-var app = express();
-// API Routes
-require('./routes')(app);
-exports["default"] = app;
