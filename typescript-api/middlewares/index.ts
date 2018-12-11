133143b4931fc7c4157cbd7415ff4f2437011048
saibot777 <stefantrajkovic777@gmail.com>
2018-12-11 15:41:46 +0100

    filtering improved

--- typescript-api/middlewares/index.ts
+++ typescript-api/middlewares/index.ts
@@ -1,6 +1,7 @@
 import { CustomRequestHandler } from "../models/request-model";
 import { RequestHandler, Request, Response, NextFunction } from "express";
 import { TourFiltersModel } from "../models/tour-filters-model";
+import { APIError } from "../models/error-model";
 
 export const authenticator: CustomRequestHandler = (req, res, next) => {
     const username = "admin";
@@ -15,6 +16,10 @@ export const logger: CustomRequestHandler = (req, res, next) => {
 
 export const apiCheckTourFilters: RequestHandler = (req: Request, res: Response, next: NextFunction) => {
     const filters = new TourFiltersModel(req.query);
-    console.log(filters);
+    for (let filter of Object.getOwnPropertyNames(req.query)) {
+        if (!filters.hasOwnProperty(filter)) {
+            next(new APIError("Query String Error", "No such filter", 400));
+        }
+    }
     next();
 }
