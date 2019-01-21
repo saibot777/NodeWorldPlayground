a54daf644cf02ab51f64a90def5389b342bb9e68
saibot777 <stefantrajkovic777@gmail.com>
2019-01-21 01:57:54 +0100

    api initiated

---
+++ instructors-app/server/api/v1/instructors/single.js
@@ -0,0 +1,13 @@
+const instructorsData = require('../../../data/instructors');
+
+module.exports = {
+  method: 'GET',
+  path: '/api/instructors/{slug}',
+  config: {
+    handler: (request, reply) => {
+      // get the specified instructor
+
+      // reply with the data
+    }
+  }
+};
---
+++ instructors-app/server/data/instructors.js
@@ -0,0 +1,64 @@
+'use strict';
+
+module.exports = [
+  {
+    id: 1,
+    name: 'Ryan Chenkie',
+    slug: 'ryan-chenkie',
+    email: 'ryanchenkie@gmail.com',
+    twitter: '@ryanchenkie',
+    github: 'chenkie',
+    courses: [
+      'Build an API with Hapi.js',
+      'Authentication for Single Page Apps'
+    ]
+  },
+  {
+    id: 2,
+    name: 'Lukas Ruebbelke',
+    slug: 'lukas-ruebbelke',
+    email: 'simpul@gmail.com',
+    twitter: '@simpulton',
+    github: 'simpulton',
+    courses: [
+      'Building Awesome Web Apps with Angular 2',
+      'Reactive Angular 2 with ngrx',
+      'Build Web Apps with Angular 2',
+      'AngularJS 1.x Application Development',
+      'Angular 1.x Essentials'
+    ]
+  },
+  {
+    id: 3,
+    name: 'Kyle Simpson',
+    slug: 'kyle-simpson',
+    email: 'getify@gmail.com',
+    twitter: '@getify',
+    github: 'getify',
+    courses: [
+      'ES6: The Right Parts',
+      'Rethinking Asynchronous JavaScript',
+      'Functional-Lite JavaScript',
+      'Organizing JavaScript Functionality',
+      'Coercion in JavaScript',
+      'Introduction to JavaScript Programming',
+      'Real-Time Web with Node.js',
+      'Advanced JavaScript',
+      'Website Performance'
+    ]
+  },
+  {
+    id: 4,
+    name: 'Brian Holt',
+    slug: 'brian-holt',
+    email: '',
+    twitter: '@holtbt',
+    github: 'btholt',
+    courses: [
+      'Complete Intro to React v2 (feat. Router v4 and Redux)',
+      'Complete Introduction to React (feat. Redux and React Router)',
+      'Four Semesters of Computer Science in 5 Hours',
+      'Choosing a JavaScript Framework'
+    ]
+  }
+];
