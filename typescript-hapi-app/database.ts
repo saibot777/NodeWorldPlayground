d19cb8d247a543fdf8546bc2d6b9b2d650c19aac
saibot777 <stefantrajkovic777@gmail.com>
2019-01-22 18:21:03 +0100

    update for mongo - removing deprecation -w

--- typescript-hapi-app/database.ts
+++ typescript-hapi-app/database.ts
@@ -12,7 +12,8 @@ export interface IDatabase {
 
 export function init(config: IDataConfiguration): IDatabase {
   (<any>Mongoose).Promise = Promise;
-  Mongoose.connect(process.env.MONGO_URL || config.connectionString);
+  Mongoose.set('useCreateIndex', true);
+  Mongoose.connect(process.env.MONGO_URL || config.connectionString, { useNewUrlParser: true } );
 
   let mongoDb = Mongoose.connection;
 
