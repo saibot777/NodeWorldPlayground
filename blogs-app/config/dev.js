a37dcb7953f2ac78e0d34de5c800cf5347724a14
saibot777 <stefantrajkovic777@gmail.com>
2019-03-06 13:55:06 +0100

    google flow impl

--- blogs-app/config/dev.js
+++ blogs-app/config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
   googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+    '769472234716-3cel4i5dm3i9polf34lo5gtm3v5jaalk.apps.googleusercontent.com',
+  googleClientSecret: 'DOOUHg0mzZwJ4I-iyAbeauTL',
+  mongoURI: 'mongodb://localhost:27017/blogs',
   cookieKey: '123123123'
 };
