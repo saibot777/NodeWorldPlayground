e0612dfaf5e643ddbbf8336c1d3d7bec826af825
saibot777 <stefantrajkovic777@gmail.com>
2019-10-14 11:27:35 +0200

    testing getUserById throw error case, impl

--- mocha-app/lib/users.test.js
+++ mocha-app/lib/users.test.js
@@ -59,5 +59,22 @@ describe("users", () => {
         done();
       });
     });
+
+    it("should catch error if there is one", done => {
+      sandbox.restore();
+      let stub = sandbox
+        .stub(mongoose.Model, "findById")
+        .yields(new Error("fake"));
+
+      users.get(123, (err, result) => {
+        expect(result).to.not.exist;
+        expect(err).to.exist;
+        expect(err).to.be.instanceOf(Error);
+        expect(stub).to.have.been.calledWith(123);
+        expect(err.message).to.equal("fake");
+
+        done();
+      });
+    });
   });
 });
